371842265ba701fb2e2e730d24b385866b898651
gouf <innocent.zero@gmail.com>
2019-12-14 05:16:58 +0900

    Implement logout function

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'static/index'
   match 'oauth/callback', via: %i[get post] # for use with Github, Facebook
   get 'oauth/:provider', to: 'oauth#oauth', as: :auth_at_provider
+  delete 'user_sessions/destroy', as: :logout
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
